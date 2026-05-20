d282ec3fb2838db571a077d2784d1b2ec664953e
tej2552 <tejkarthik@gmail.com>
2026-05-20 13:01:23 +0530

    automated a test which checks the query response

--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -1,13 +1,26 @@
 import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class APITests {
 
     SpecBuilder specBuilder = new SpecBuilder();
+    Payload payload = new Payload();
 
     @Test
-    public void shouldFetchData(){
+    public void shouldFetchData() throws IOException {
+
+        Map<String, String> variables = new HashMap<>();
+        variables.put("gameId", payload.extractVariables("getGameById").trim());
+
         RestAssured.given()
-                .spec(specBuilder.buildRequestSpec());
+                .spec(specBuilder.buildRequestSpec())
+                .body(payload.buildPayload("getGameById", variables)).when()
+                .post()
+                .then().statusCode(200)
+                .log().all();
     }
 }
--- src/test/java/Payload.java
+++ src/test/java/Payload.java
@@ -1,10 +1,54 @@
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class Payload {
 
-    public void buildPayload(){
+    public Map<String, Object> buildPayload(String str, Map<String, String> variables) throws IOException {
+        Map<String, Object> payload = new HashMap<>();
 
+        payload.put("query", extractQuery(str));
+        payload.put("variables", variables);
+
+        return payload;
     }
 
-    public void checkField(String field){
+    public String extractQuery(String str) throws IOException {
+        String content = Files.readString(Path.of("src/main/java/queries/queries.graphql"));
 
+        String[] blocks = content.split("#name:");
+
+
+        for(String block : blocks){
+            block = block.trim();
+            if(block.trim().startsWith(str)){
+                String[] lines = block.split("\n", 3);
+                return lines[2].trim();
+            }
+        }
+
+        throw new RuntimeException("Query Not Found");
+    }
+
+    public String extractVariables(String str) throws IOException {
+        String content = Files.readString(Path.of("src/main/java/queries/queries.graphql"));
+
+        String[] blocks = content.split("#name:");
+
+        for(String block : blocks){
+            block = block.trim();
+            if(block.trim().startsWith(str)){
+                String[] lines = block.split("\n", 3);
+                return lines[1].trim().split(":")[1];
+            }
+        }
+
+        throw new RuntimeException("Query Not Found");
     }
 }
